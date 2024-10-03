2ee7e4f0cc5db1739ed9cba6d3ee7003c8d8494d
jayanth-ctrl <66643469+jayanth-ctrl@users.noreply.github.com>
2024-10-03 16:16:54 -0400

    Update testservicesbot.py

--- testservicesbot.py
+++ testservicesbot.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import fitz  
 import os
 import io
@@ -7,7 +11,6 @@ import random
 import time
 import warnings
 import numpy as np
-import sqlite3
 import streamlit as st
 from PIL import Image
 from sentence_transformers import SentenceTransformer
