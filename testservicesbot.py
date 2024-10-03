5d767909fc539888f9b65e38a73eca057a88d148
jayanth-ctrl <66643469+jayanth-ctrl@users.noreply.github.com>
2024-10-03 15:59:59 -0400

    Update testservicesbot.py

--- testservicesbot.py
+++ testservicesbot.py
@@ -1,6 +1,6 @@
 __import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import pysqlite3
+sys.modules['sqlite3'] = sys.modules["pysqlite3"]
 
 import fitz  
 import os
